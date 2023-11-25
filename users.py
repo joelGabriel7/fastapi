60b22c3cad54b319e257867ac3b9e25263eaf360
Joel <joelgerman671@gmail.com>
2023-11-25 16:33:52 -0400

    CRUD operations ready

--- users.py
+++ users.py
@@ -29,6 +29,41 @@ async def user(id: int):
     return search_users(id)
 
 
+# Post
+
+@app.post('/user/')
+async def new_user(user: User):
+    if type(search_users(user.id)) == User:
+        return {"Error" : "El usuario ya existe"}
+    else:
+        users_list.append(user)
+    return user    
+
+
+@app.put('/user/')
+async def user(user:User):
+    found = False
+    for index, saved_user in enumerate(users_list):
+        if saved_user.id == user.id:
+            users_list[index] = user
+            found = True
+    return user        
+    if not found:
+         return {"error": 'Usuario no encontrado'}
+
+#Delete
+
+@app.delete('/user/{id}')
+async def user_delete(id: int):
+    found = False
+    for index, saved_user in enumerate(users_list):
+        if saved_user.id == id:
+            del users_list[index] 
+            found = True
+            break
+    if not found:
+        return {"error": 'No se ha eliminado el usuario'}
+
 def search_users(id: int):
     users = filter(lambda user: user.id == id, users_list)
     try:
